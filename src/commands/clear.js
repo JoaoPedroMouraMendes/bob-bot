616397975da90949c1a0f31da05bdc1ebae4924d
João Pedro <joaopedromouramendes1@gmail.com>
2023-11-27 15:27:56 -0300

    Comandos de limpar mensagens atualizado

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -6,7 +6,7 @@ const permissions = new Permissions();
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("clear-messages")
+        .setName("delete-messages")
         .setDescription("Exclui mensagens")
         .addSubcommand(subcommand =>
             subcommand
@@ -15,7 +15,7 @@ module.exports = {
         )
         .addSubcommand(subcommand =>
             subcommand
-                .setName("by-username")
+                .setName("user")
                 .setDescription("Exclui mensagens de uma usuário especifico")
                 .addUserOption(option =>
                     option
@@ -23,6 +23,16 @@ module.exports = {
                         .setDescription("Usuário que tera suas mensagens excluidas")
                         .setRequired(true)
                 )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("only-users")
+                .setDescription("Exclui todas as mensagens de usuários")
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("only-bots")
+                .setDescription("Exclui todas as mensagens de bots")
         ),
 
     async execute({ interaction }) {
@@ -45,34 +55,51 @@ module.exports = {
         //* Subcomandos
         switch (interaction.options.getSubcommand()) {
             // Exclui todas as mensagens do canal
-            case "all":
-
-                // Resposta
-                const successEmbed = new EmbedBuilder()
-                    .setColor(pallete.success)
-                    .setDescription("Limpando...");
-                await interaction.reply({ embeds: [successEmbed] });
+            case "all": {
+                await interaction.deferReply();
                 // Busca e deleta as mensagens
                 const messages = await channel.messages.fetch();
-                channel.bulkDelete(messages);
+                await channel.bulkDelete(messages);
 
                 break;
+            }
             // Exclui todas as mensagens de um usuário
-            case "by-username":
+            case "user": {
+                const reply = await interaction.deferReply();
+                const author = await interaction.options.getUser("user", true);
 
-            const author = await interaction.options.getUser("user", true);
-                // Resposta
-                const successEmbed2 = new EmbedBuilder()
-                    .setColor(pallete.success)
-                    .setDescription(`Limpando mensagens de ${author.toString()}...`);
-                await interaction.reply({ embeds: [successEmbed2] });
-
-                // Busca e deleta as mensagens
-                const messages2 = await channel.messages.fetch();
-                const filteredMessages = messages2.filter(message => message.author.id === author.id);
+                // Busca, filtra e deleta as mensagens
+                const messages = await channel.messages.fetch();
+                const filteredMessages = messages.filter
+                    (message => message.author.id === author.id ||
+                        message.interaction.id === reply.interaction.id);
                 channel.bulkDelete(filteredMessages);
 
                 break;
+            }
+            // Exclui todas as mensagens de usuários
+            case "only-users": {
+                const reply = await interaction.deferReply();
+
+                // Busca, filtra e deleta as mensagens
+                const messages = await channel.messages.fetch();
+                const filteredMessages = messages.filter
+                    (message => !message.author.bot || message.interaction.id === reply.interaction.id);
+                channel.bulkDelete(filteredMessages);
+
+                break;
+            }
+            // Exclui todas as mensagens de bots
+            case "only-bots": {
+                // Resposta
+                await interaction.deferReply();
+
+                // Busca, filtra e deleta as mensagens
+                const messages = await channel.messages.fetch();
+                const filteredMessages = messages.filter(message => message.author.bot);
+                channel.bulkDelete(filteredMessages);
+                break;
+            }
         }
     }
 }
